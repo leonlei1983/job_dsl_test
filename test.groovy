77d90ac27fb3eef5581c86dcde864c1a1d4c4ca9
Leon Lei <leon.lei@@gogolook.com>
2020-03-27 10:39:58 +0800

    update content

--- test.groovy
+++ test.groovy
@@ -2,3 +2,24 @@
 def env = "${LEI_ENV}".toLowerCase();
 def migration = "${LEI_MIGRATION ?: 'false'}".toBoolean();
 
+this.pipelineJob('example') {
+    environmentVariables {
+
+    }
+    parameters {
+
+    }
+    properties {
+
+    }
+    triggers {
+
+    }
+    throttleConcurrentBuilds {
+
+    }
+    logRotator {
+        numToKeep(30)
+    }
+    disabled()
+}
