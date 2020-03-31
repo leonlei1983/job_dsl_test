831333476adf3232d332c7fe7ed746a443f1b079
Leon Lei <leon.lei@@gogolook.com>
2020-03-31 15:17:37 +0800

    update content

--- test.groovy
+++ test.groovy
@@ -1,28 +1,59 @@
-def migration = "${LEI_MIGRATION}".toBoolean();
+class PipeJob {
+    boolean disable
 
-this.pipelineJob('example') {
-    environmentVariables {
-        envs(['env': 'staging', 'mode': 'jenkins'])
+    def PipeJob() {
+
     }
-    // parameters {
 
-    // }
-    properties {
-        disableConcurrentBuilds()
-        pipelineTriggers {
-            triggers {
-                githubPush()
+    def disable() {
+        disable = true
+    }
+
+
+    static def run(factory, name, closure) {
+        def job = new PipeJob()
+        closure.resolveStrategy = Closure.DELEGATE_FIRST
+        closure.delegate = job
+        closure()
+
+        factory.pipelineJob(name) {
+            environmentVariables {
+                envs(['env': 'staging', 'mode': 'jenkins'])
+            }
+            // parameters {
+
+            // }
+            properties {
+                disableConcurrentBuilds()
+                pipelineTriggers {
+                    triggers {
+                        githubPush()
+                    }
+                }
+            }
+            // triggers {
+            //     upstream(pipeline.upstream)
+            // }
+            // throttleConcurrentBuilds {
+
+            // }
+            logRotator {
+                numToKeep(30)
+            }
+            if (job.disable) {
+                disabled()
             }
         }
     }
-    // triggers {
-    //     upstream(pipeline.upstream)
-    // }
-    // throttleConcurrentBuilds {
-
-    // }
-    logRotator {
-        numToKeep(30)
-    }
-    disabled()
 }
+
+def pipeJobGen = {name, closure ->
+    PipeJob.run this, name, closure 
+}
+
+pipeJobGen("example2") {
+    
+}
+
+
+
