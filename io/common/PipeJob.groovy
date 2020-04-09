a079cf6461729d316278c3516e0ed6b7953c9c8f
Leon Lei <leon.lei@@gogolook.com>
2020-04-09 18:56:47 +0800

    test jenkins build

--- io/common/PipeJob.groovy
+++ io/common/PipeJob.groovy
@@ -27,12 +27,72 @@ class PipeJob {
             //     booleanParam "open", false
             // }
 
+            definition {
+                cpsScm {
+                    lightweight(false)
+                    scm {
+                        git {
+                            remote {
+                                github("Gogolook-Inc/${pipeline.github.repo}", 'ssh')
+                                // credentials('serverdeploy_key')
+                                refspec('+refs/pull/${ghprbPullId}/*:refs/remotes/origin/pr/${ghprbPullId}/*')
+                            }
+                            branch('refs/heads/master')
+                            extensions {
+                                pruneBranches()
+                                wipeOutWorkspace()
+                                pathRestriction {
+                                    excludedRegions('')
+                                    includedRegions("^aaa/.*")
+                                }
+                                sparseCheckoutPaths {
+                                    sparseCheckoutPaths {
+                                        sparseCheckoutPath {
+                                            path(pipeline.defs.scriptFolder)
+                                        }
+                                    }
+                                }
+                            }
+                            properties {
+                                disableConcurrentBuilds()
+                            }
+                        }
+                    }
+                    scriptPath('aaa')
+                }
+            }
+
             properties {
                 disableConcurrentBuilds()
                 pipelineTriggers {
-                    // triggers {
-                    //     githubPush()
-                    // }
+                    triggers {
+                        // githubPush()
+                        ghprbTrigger {
+                            useGitHubHooks(true)
+                            permitAll(true)
+                            allowMembersOfWhitelistedOrgsAsAdmin(false)
+                            adminlist('')
+                            whitelist('')
+                            orgslist('')
+                            // cron('H/5 * * * *')
+                            triggerPhrase('')
+                            onlyTriggerPhrase(false)
+                            autoCloseFailedPullRequests(false)
+                            displayBuildErrorsOnDownstreamBuilds(false)
+                            commentFilePath('')
+                            skipBuildPhrase('.*\\[skip\\W+ci\\].*')
+                            blackListCommitAuthor('')
+                            msgSuccess('')
+                            msgFailure('')
+                            commitStatusContext('')
+                            gitHubAuthId('')
+                            buildDescTemplate('')
+                            blackListLabels('')
+                            whiteListLabels('')
+                            excludedRegions('')
+                            includedRegions("^aaa/.*")
+                        }
+                    }
                 }
             }
 
